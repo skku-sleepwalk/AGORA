03a478495ea281944d03570481426cac0392d0b7
skku-sleepwalk <lucas7211@g.skku.edu>
2023-06-21 23:49:13 +0900

    profile added

---
+++ frontend/components/WriteWriting/WriteWriting.styles.tsx
@@ -0,0 +1,5 @@
+import { createStyles } from "@mantine/core";
+
+export const WriteWritingStyles = createStyles((theme) => ({
+
+}));
---
+++ frontend/components/WriteWriting/WriteWriting.tsx
@@ -0,0 +1,17 @@
+import { Container } from "@mantine/core"
+import { Avatar } from "@mantine/core"
+
+function WriteWriting(){
+    return(
+        <Container>
+            hehehe
+            aaa
+            <Avatar src={"https://avatars.githubusercontent.com/u/52057157?v=4"} radius="xl" size={30} />
+            <span>
+                kkk 
+            </span>
+        </Container>
+
+    )
+}
+export default WriteWriting
