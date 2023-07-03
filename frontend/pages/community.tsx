2e1db2c8a0ba784798ebaf26143dac73099775fe
skku-sleepwalk <lucas7211@g.skku.edu>
2023-07-03 21:47:49 +0900

    leftlayout

--- frontend/pages/community.tsx
+++ frontend/pages/community.tsx
@@ -1,9 +1,11 @@
 import CommunityLayout from "../components/pages/community/CommunityLayout/CommunityLayout";
+import { LeftSidebar } from "../components/pages/community/LeftSidebar/LeftSidebar";
 import { SideBar } from "../components/pages/community/sidebar/SideBar";
 
 function Main() {
   return (
     <CommunityLayout
+      leftSection={<LeftSidebar></LeftSidebar>}
       rightSection={
         <SideBar
           onSearchSubmit={(text) => {
