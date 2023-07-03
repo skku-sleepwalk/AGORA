dff76a451b727e5d3c5dffb1c1d34b44ddad12e8
skku-sleepwalk <lucas7211@g.skku.edu>
2023-07-03 21:38:03 +0900

    submit added

---
+++ frontend/pages/community.tsx
@@ -0,0 +1,18 @@
+import CommunityLayout from "../components/pages/community/CommunityLayout/CommunityLayout";
+import { SideBar } from "../components/pages/community/sidebar/SideBar";
+
+function Main() {
+  return (
+    <CommunityLayout
+      rightSection={
+        <SideBar
+          onSearchSubmit={(text) => {
+            console.log(text);
+          }}
+        />
+      }
+    />
+  );
+}
+
+export default Main;
