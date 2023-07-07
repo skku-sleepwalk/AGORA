2c25e75e2d025ece7efabd92d387fec3056722b4
BestSanghyeon <qazxsw100415@gmail.com>
2023-07-07 17:37:52 +0900

    data => { data } 수정

--- frontend/utils/api/upload.ts
+++ frontend/utils/api/upload.ts
@@ -5,7 +5,7 @@ import { PostUploadImageResponse } from "../../types/api/upload";
 export async function uploadImage(image: File): Promise<PostUploadImageResponse> {
   const formData = new FormData();
   formData.append("image", image);
-  const data = await axios.post<FormData, PostUploadImageResponse>(
+  const { data } = await axios.post<FormData, AxiosResponse<PostUploadImageResponse>>(
     "http://localhost:8000/upload/image",
     formData,
     {
