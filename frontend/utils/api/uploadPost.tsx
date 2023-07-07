0466449428c4ea71436f3ed7b2be8f5966f931d6
skku-sleepwalk <lucas7211@g.skku.edu>
2023-07-07 20:22:08 +0900

    저장 안했었음 ㅋㅋ

---
+++ frontend/utils/api/uploadPost.tsx
@@ -0,0 +1,17 @@
+import axios, { AxiosResponse } from "axios";
+import { PostBoardResponse } from "../../types/api/boards";
+
+export async function uploadPost(Post: File): Promise<PostBoardResponse> {//기존 promise 함수의 제네릭을 사용!
+  const formData = new FormData();  //일종의 form이다.
+  formData.append("Post", Post); //form 안에 key"Post" value Post 넣을거임!
+  const { data } = await axios.post<FormData, AxiosResponse<PostBoardResponse>>(
+    "http://localhost:8000/boards/Post",
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
+  return data;
+}
