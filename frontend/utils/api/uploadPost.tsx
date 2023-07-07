22f75cdaa4efe6b91bd6dcaf79294098b4685138
skku-sleepwalk <lucas7211@g.skku.edu>
2023-07-07 20:23:07 +0900

    주석 추가

--- frontend/utils/api/uploadPost.tsx
+++ frontend/utils/api/uploadPost.tsx
@@ -1,17 +1,18 @@
 import axios, { AxiosResponse } from "axios";
 import { PostBoardResponse } from "../../types/api/boards";
 
-export async function uploadPost(Post: File): Promise<PostBoardResponse> {//기존 promise 함수의 제네릭을 사용!
-  const formData = new FormData();  //일종의 form이다.
+export async function uploadPost(Post: File): Promise<PostBoardResponse> {
+  //기존 promise 함수의 제네릭을 사용!
+  const formData = new FormData(); //일종의 form이다.
   formData.append("Post", Post); //form 안에 key"Post" value Post 넣을거임!
   const { data } = await axios.post<FormData, AxiosResponse<PostBoardResponse>>(
-    "http://localhost:8000/boards/Post",
-    formData,
+    "http://localhost:8000/boards/Post", //URL
+    formData, //data
     {
       headers: {
         "Content-Type": "multipart/form-data",
       },
-    }
+    } //config
   );
   return data;
 }
