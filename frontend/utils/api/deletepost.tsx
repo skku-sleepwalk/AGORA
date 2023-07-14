e33172639199e534c22a17806aa0bba6afba8019
skku-sleepwalk <lucas7211@g.skku.edu>
2023-07-14 22:10:07 +0900

    delete함수?

---
+++ frontend/utils/api/deletepost.tsx
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+export default async function deletePost(postId: number): Promise<void> {
+  try {
+    const response: AxiosResponse = await axios.delete(
+      `http://localhost:8000/developer-community-boards${postId}`
+    );
+    console.log(response.data); // 삭제된 포스트의 응답 데이터 처리
+
+    // 성공적으로 삭제되었을 때 추가로 작업을 수행할 수 있습니다.
+    // 예: 상태 업데이트, 다른 요청 등
+  } catch (error) {
+    console.error(error); // 오류 처리
+  }
+}
