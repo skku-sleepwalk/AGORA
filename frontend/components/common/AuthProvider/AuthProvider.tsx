186fcc86ec819a5c86253021d481a6ba78d4a52c
BestSanghyeon <qazxsw100415@gmail.com>
2023-07-18 10:28:49 +0900

    user 불러와서 개발시 사용할 수 있도록 세팅

--- frontend/components/common/AuthProvider/AuthProvider.tsx
+++ frontend/components/common/AuthProvider/AuthProvider.tsx
@@ -1,5 +1,6 @@
 import { User } from "../../../types/api/user";
 import { ReactNode, createContext, useState } from "react";
+import useSWR from "swr";
 
 interface AuthContextType {
   user?: User;
@@ -15,33 +16,13 @@ export interface AuthProviderProps {
 function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User | null>(null);
   const [token, setToken] = useState<string | null>(null);
+  const { data } = useSWR<User[]>("http://localhost:8000/users");
 
   return (
     <AuthContext.Provider
       value={{
-        user: {
-          id: "fab03f34-4752-4a83-8fac-ebebb81c6952",
-          name: "반야",
-          description: "쉬고 싶은 레이디",
-          email: "04smailing@naver.com",
-          token: 0,
-          rating: 0,
-          createdAt: "2023-07-13T20:49:47.705Z",
-          updatedAt: "2023-07-13T20:49:47.705Z",
-          deletedAt: null,
-        },
-        // {
-        //   id: "d640c184-c7ba-4bf2-a552-aedb30474643",
-        //   name: "루카스",
-        //   description: null,
-        //   email: "lucas@naver.com",
-        //   token: 0,
-        //   rating: 0,
-        //   createdAt: "2023-07-13T20:49:47.705Z",
-        //   updatedAt: "2023-07-13T20:49:47.705Z",
-        //   deletedAt: null
-        // },
-        token: "04smailing@naver.com",
+        user: data?.[0],
+        token: data?.[0]?.email,
       }}
     >
       {children}
