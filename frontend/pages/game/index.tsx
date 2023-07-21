88a03dc46115fcd7308065e12b09a211c8211fdc
BanYeah <126534852+BanYeah@users.noreply.github.com>
2023-07-22 01:46:59 +0900

    게임 상세 대충 완

--- frontend/pages/game/index.tsx
+++ frontend/pages/game/index.tsx
@@ -1,4 +1,5 @@
 import CardContainer from "../../components/common/CardContainer/CardContainer";
+import { GameInfo } from "../../components/pages/game/GameInfo/GameInfo";
 import GameLayout from "../../components/pages/game/GameLayout/GameLayout";
 import { MainCarousel } from "../../components/pages/game/MainCarousel/MainCarousel";
 import MainLayout from "../../components/pages/game/MainLayout/MainLayout";
@@ -14,11 +15,7 @@ function Game() {
 
     <GameLayout
       photoSection={<MainCarousel isInfo={true} />}
-      summarySection={
-        <CardContainer w={"100%"} h={"100%"}>
-          summary
-        </CardContainer>
-      }
+      InfoSection={<GameInfo />}
       tapSection={
         <CardContainer w={"100%"} h={"100%"}>
           tap
