14981c5430f990dd3148c81a4fc31be39dd918d5
BanYeah <126534852+BanYeah@users.noreply.github.com>
2023-07-21 22:44:56 +0900

    게임 레이아웃 대충

--- frontend/components/pages/game/GameLayout/GameLayout.tsx
+++ frontend/components/pages/game/GameLayout/GameLayout.tsx
@@ -1,18 +1,30 @@
 import { useGameLayoutStyles } from "./GameLayout.styles";
 
 export interface CommunityLayoutProps {
-  children?: React.ReactNode;
+  photoSection?: React.ReactNode;
+  summarySection?: React.ReactNode;
   tapSection?: React.ReactNode;
+  children?: React.ReactNode;
   rightSection?: React.ReactNode;
 }
 
-function GameLayout({ children, tapSection, rightSection }: CommunityLayoutProps) {
+function GameLayout({
+  photoSection,
+  summarySection,
+  tapSection,
+  children,
+  rightSection,
+}: CommunityLayoutProps) {
   const { classes } = useGameLayoutStyles();
 
   return (
     <div className={classes.container}>
+      <div className={classes.topContainer}>
+        <div className={classes.PhotoContainer}>{photoSection}</div>
+        <div className={classes.summaryContainer}>{summarySection}</div>
+      </div>
       <div className={classes.tapContainer}>{tapSection}</div>
-      <div className={classes.rightMainContainer}>
+      <div className={classes.bottomContainer}>
         <div className={classes.mainContainer}>{children}</div>
         <div className={classes.rightContainer}>{rightSection}</div>
       </div>
