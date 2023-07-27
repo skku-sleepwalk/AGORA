bf8fd39f2e5b07b841fdc008b1e080ef9e1980c5
BanYeah <126534852+BanYeah@users.noreply.github.com>
2023-07-27 22:12:41 +0900

    게임 소식 중간 저장

---
+++ frontend/components/pages/game/GameNewsSection/GameNewsSection.styles.tsx
@@ -0,0 +1,9 @@
+import { createStyles } from "@mantine/core";
+
+export const useGameNewsSectionStyles = createStyles((theme) => ({
+  reviewSection: {
+    width: "100%",
+    height: "100%",
+    padding: "2rem 1rem",
+  },
+}));
