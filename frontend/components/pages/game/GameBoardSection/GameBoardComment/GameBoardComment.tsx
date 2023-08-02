7b51d300ba4b297f43d16395b2dff1a38d05d05c
BanYeah <126534852+BanYeah@users.noreply.github.com>
2023-08-02 22:57:07 +0900

    수정 버튼 추가

--- frontend/components/pages/game/GameBoardSection/GameBoardComment/GameBoardComment.tsx
+++ frontend/components/pages/game/GameBoardSection/GameBoardComment/GameBoardComment.tsx
@@ -4,6 +4,7 @@ import {
   Button,
   Divider,
   Group,
+  Menu,
   Stack,
   Text,
   TypographyStylesProvider,
@@ -13,11 +14,14 @@ import {
 import { useGameBoardCommentStyles } from "./GameBoardComment.styles";
 import { MOCKUP_CONTENT } from "../../../../../mockups/post";
 import {
-  IconMessages,
+  IconBell,
+  IconDotsVertical,
+  IconPencil,
   IconThumbDown,
   IconThumbDownFilled,
   IconThumbUp,
   IconThumbUpFilled,
+  IconTrash,
 } from "@tabler/icons-react";
 import { useDisclosure, useMediaQuery, useSetState } from "@mantine/hooks";
 import { useState } from "react";
@@ -52,8 +56,7 @@ export function GameBoardComment() {
   };
 
   // 답글 관련
-  const [opened, { toggle }] = useDisclosure(false);
-  const canReview = false;
+  const canEdit = true;
 
   return (
     <Box>
@@ -110,27 +113,62 @@ export function GameBoardComment() {
           )}
         </Stack>
         {/* 후기 하단 버튿들 */}
-        <Group className={classes.marginLeft}>
-          <InvisibleButton onClick={handleGoodState}>
-            <Group spacing={"0.3rem"}>
-              {goodBadstate.good ? (
-                <IconThumbUpFilled stroke={1.5} size={smallScreen ? "1rem" : "1.5rem"} />
-              ) : (
-                <IconThumbUp stroke={1.5} size={smallScreen ? "1rem" : "1.5rem"} />
+        <Group className={classes.marginLeft} position="apart">
+          <Group>
+            <InvisibleButton onClick={handleGoodState}>
+              <Group spacing={"0.3rem"}>
+                {goodBadstate.good ? (
+                  <IconThumbUpFilled stroke={1.5} size={smallScreen ? "1rem" : "1.5rem"} />
+                ) : (
+                  <IconThumbUp stroke={1.5} size={smallScreen ? "1rem" : "1.5rem"} />
+                )}
+                <Text fz={14}>2</Text>
+              </Group>
+            </InvisibleButton>
+            <InvisibleButton onClick={handleBadState}>
+              <Group spacing={"0.3rem"}>
+                {goodBadstate.bad ? (
+                  <IconThumbDownFilled stroke={1.5} size={smallScreen ? "1rem" : "1.5rem"} />
+                ) : (
+                  <IconThumbDown stroke={1.5} size={smallScreen ? "1rem" : "1.5rem"} />
+                )}
+                <Text fz={14}>1</Text>
+              </Group>
+            </InvisibleButton>
+          </Group>
+          <Menu shadow="md" width={120} position="bottom-end" offset={1}>
+            <Menu.Target>
+              <UnstyledButton className={classes.dotButton}>
+                <IconDotsVertical />
+              </UnstyledButton>
+            </Menu.Target>
+            <Menu.Dropdown>
+              {!canEdit && (
+                <Menu.Item icon={<IconBell size={18} stroke={2} />} className={classes.menuItem}>
+                  신고하기
+                </Menu.Item>
               )}
-              <Text fz={14}>2</Text>
-            </Group>
-          </InvisibleButton>
-          <InvisibleButton onClick={handleBadState}>
-            <Group spacing={"0.3rem"}>
-              {goodBadstate.bad ? (
-                <IconThumbDownFilled stroke={1.5} size={smallScreen ? "1rem" : "1.5rem"} />
-              ) : (
-                <IconThumbDown stroke={1.5} size={smallScreen ? "1rem" : "1.5rem"} />
+              {canEdit && (
+                <>
+                  <Menu.Item
+                    onClick={() => {}}
+                    icon={<IconPencil size={18} stroke={2} />}
+                    className={classes.menuItem}
+                  >
+                    수정하기
+                  </Menu.Item>
+                  <Menu.Divider />
+                  <Menu.Item
+                    onClick={() => {}}
+                    icon={<IconTrash size={18} stroke={2} />}
+                    className={classes.menuItem}
+                  >
+                    삭제하기
+                  </Menu.Item>
+                </>
               )}
-              <Text fz={14}>1</Text>
-            </Group>
-          </InvisibleButton>
+            </Menu.Dropdown>
+          </Menu>
         </Group>
       </Stack>
     </Box>
