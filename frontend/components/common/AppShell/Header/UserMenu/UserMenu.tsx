c0812e9fe84115472e0f7cad65128c1972c2e35f
BanYeah <126534852+BanYeah@users.noreply.github.com>
2023-08-07 14:54:15 +0900

    마이페이지 이동

--- frontend/components/common/AppShell/Header/UserMenu/UserMenu.tsx
+++ frontend/components/common/AppShell/Header/UserMenu/UserMenu.tsx
@@ -1,6 +1,7 @@
 import { Group, Avatar, Text, Button, Menu } from "@mantine/core";
 import { IconChevronDown, IconSettings, IconUser, IconLogout } from "@tabler/icons-react";
 import { useUserMenuStyles } from "./UserMenu.styles";
+import { useRouter } from "next/router";
 
 export interface UserMenuProps {
   image: string;
@@ -9,6 +10,7 @@ export interface UserMenuProps {
 
 export function UserMenu({ image, name }: UserMenuProps) {
   const { classes } = useUserMenuStyles();
+  const router = useRouter();
 
   return (
     <Menu shadow="md" width={200}>
@@ -26,7 +28,14 @@ export function UserMenu({ image, name }: UserMenuProps) {
 
       <Menu.Dropdown>
         <Menu.Label>메뉴</Menu.Label>
-        <Menu.Item icon={<IconUser />}>마이페이지</Menu.Item>
+        <Menu.Item
+          icon={<IconUser />}
+          onClick={() => {
+            router.replace("http://localhost:3000/mypage");
+          }}
+        >
+          마이페이지
+        </Menu.Item>
         <Menu.Item icon={<IconSettings />}>설정</Menu.Item>
         <Menu.Item icon={<IconLogout />}>로그아웃</Menu.Item>
       </Menu.Dropdown>
