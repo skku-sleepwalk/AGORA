f64a01adaee09f6e77606b8f19aaf2cf54961c77
mnseok <min.seok@g.skku.edu>
2023-08-10 14:08:26 +0900

    Update user.module.ts

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -4,10 +4,13 @@ import { UsersController } from './controllers/user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entites/user.entity';
 import { PlayTime } from 'src/entites/game/game.playtime.entity';
+import { UserSubscribe } from 'src/entites/user.subscribe.entity';
+import { UserSubscribeController } from './controllers/user.subscribe.controller';
+import { UserSubscribeService } from './services/user.subscribe.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, PlayTime])],
-  providers: [UserService],
-  controllers: [UsersController],
+  imports: [TypeOrmModule.forFeature([User, PlayTime, UserSubscribe])],
+  providers: [UserService, UserSubscribeService],
+  controllers: [UsersController, UserSubscribeController],
 })
 export class UsersModule {}
