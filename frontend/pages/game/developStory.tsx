772669586b8a91b89ecec7948e4a3b2cc3c64a09
skku-sleepwalk <lucas7211@g.skku.edu>
2023-08-10 21:11:58 +0900

    서브헤더

--- frontend/pages/game/developStory.tsx
+++ frontend/pages/game/developStory.tsx
@@ -1,9 +1,14 @@
 import BestStories from "../../components/pages/game/DevelopStories/BestStories";
 import { DevelopStories } from "../../components/pages/game/DevelopStories/DevelopStories";
 import Stories from "../../components/pages/game/DevelopStories/Stories";
+import { MainTab } from "../../components/pages/game/MainTab/MainTab";
 export default function DevelopStory() {
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
+      <div style={{ position: "sticky", top: "70px", zIndex: "1000" }}>
+        <MainTab active="develop"></MainTab>
+      </div>
+
       <DevelopStories>
         <Stories></Stories>
       </DevelopStories>
