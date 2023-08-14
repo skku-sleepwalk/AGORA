11585e53432cd7605c771918d787fc769d06c42f
BanYeah <126534852+BanYeah@users.noreply.github.com>
2023-08-14 23:51:12 +0900

    에셋

---
+++ frontend/components/pages/asset/MainAssetSection/MainAsset/MainAsset.styles.tsx
@@ -0,0 +1,16 @@
+import { createStyles } from "@mantine/core";
+
+export const useMainAssetStyles = createStyles((theme) => ({
+  wrapper: {
+    width: "10rem",
+    height: "16.9rem",
+    marginBottom: "1rem",
+    padding: "0.5rem",
+  },
+
+  button: {
+    width: "1.6rem",
+    height: "1.6rem",
+    padding: "0.3rem",
+  },
+}));
