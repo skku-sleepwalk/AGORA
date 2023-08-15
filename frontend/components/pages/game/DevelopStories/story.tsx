28dfc7702adde2c0ff15f3037fe97d31ccb7c7dd
skku-sleepwalk <lucas7211@g.skku.edu>
2023-08-15 16:17:39 +0900

    이미지도

--- frontend/components/pages/game/DevelopStories/story.tsx
+++ frontend/components/pages/game/DevelopStories/story.tsx
@@ -3,21 +3,32 @@ import { Image } from "@mantine/core";
 import { Text } from "@mantine/core";
 import { IconMessage, IconHeart, Icon123, IconShare, IconBookmark } from "@tabler/icons-react";
 import { GameBoard } from "../../../../types/api/game/gameBoard";
+import { extractImageSrc } from "../../../../utils/api/ViewPhotos";
 export interface StoryProps {
   story: GameBoard;
 }
 export default function Story({ story }: StoryProps) {
+  const thumbnailUrl = extractImageSrc(story.content)[0];
   let removeImgTag = removeImgTags(story.content);
   removeImgTag = removeImgTag.replace(/<\/?p[^>]*>/g, "");
   return (
     <div style={{ display: "flex", flexDirection: "column", width: "100%", height: "100%" }}>
-      <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "140px",
+        }}
+      >
         <Image
           radius={"md"}
           width={"85%"}
           height={"50%"}
           src={
-            "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA3MTNfMTAz%2FMDAxNjg5MjUzNjY3NDQz.c6VImpgJ0qy1bRZGHiZVxfkRlqoW6UG1KY6MySFz8Acg.ZOb1WGfiq3B4pdgr8Gj272XBX_pe3BKODGBPRGMzGUUg.JPEG.wltndk97%2Foutput_4028971259.jpg&type=sc960_832"
+            thumbnailUrl == ""
+              ? "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA3MTNfMTAz%2FMDAxNjg5MjUzNjY3NDQz.c6VImpgJ0qy1bRZGHiZVxfkRlqoW6UG1KY6MySFz8Acg.ZOb1WGfiq3B4pdgr8Gj272XBX_pe3BKODGBPRGMzGUUg.JPEG.wltndk97%2Foutput_4028971259.jpg&type=sc960_832"
+              : thumbnailUrl
           }
         ></Image>
       </div>
