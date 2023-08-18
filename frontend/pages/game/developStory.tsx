a3e87b21069bc0e11a13362a4b79615efa698da8
skku-sleepwalk <lucas7211@g.skku.edu>
2023-08-18 15:53:37 +0900

    Link issue 해결

--- frontend/pages/game/developStory.tsx
+++ frontend/pages/game/developStory.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import BestStories from "../../components/pages/game/DevelopStories/BestStories";
 import { DevelopStories } from "../../components/pages/game/DevelopStories/DevelopStories";
 import Stories from "../../components/pages/game/DevelopStories/Stories";
 import { MainTab } from "../../components/pages/game/MainTab/MainTab";
 export default function DevelopStory() {
+  const GameID = "6a05a155-147b-4112-a95f-88e53edec2aa";
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <div style={{ position: "sticky", top: "70px", zIndex: "1000" }}>
@@ -10,7 +12,17 @@ export default function DevelopStory() {
       </div>
 
       <DevelopStories TitleProp="임시용 개발일지 몰아보기">
-        <Stories gameID={"6a05a155-147b-4112-a95f-88e53edec2aa"}></Stories>
+        <Link
+          href={GameID}
+          style={{
+            textDecoration: "none",
+            color: "black",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Stories gameID={GameID}></Stories>
+        </Link>
       </DevelopStories>
       <DevelopStories TitleProp="Best of Develop Stories">
         <BestStories></BestStories>
